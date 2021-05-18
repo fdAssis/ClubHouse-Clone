8ed331c4ece5562684cdeeef7787b7cfa249fb58
fdAssis <fd07ass@gmail.com>
2021-05-18 17:18:12 -0300

    init server

---
+++ server/src/util/socket.js
@@ -0,0 +1,41 @@
+import http from 'http';
+import { Server } from 'socket.io';
+
+export default class SocketServer {
+  #io;
+
+  constructor({ port }) {
+    this.port = port;
+  }
+
+  async start() {
+    const server = http.createServer((request, response) => {
+      response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+      });
+    });
+
+    this.#io = new Server(server, {
+      cors: {
+        origin: '*',
+        credentials: false,
+      },
+    });
+
+    const room = this.#io.of('/room');
+    room.on('connection', (socket) => {
+      socket.emit('userConnection', 'socket is se conectou' + socket.id);
+
+      socket.on('joinRoom', (dados) => {
+        console.log('dados recebidos', dados);
+      });
+    });
+
+    return new Promise((resolve, reject) => {
+      server.on('error', reject);
+
+      server.listen(this.port, () => resolve(server));
+    });
+  }
+}
